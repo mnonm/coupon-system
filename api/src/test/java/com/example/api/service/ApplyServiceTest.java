167dafb29f14df3a455279ad35ef740071c85b84
songseongbin <ppsosqq@gmail.com>
2024-10-23 10:03:09 +0900

    [test] 멀티스레드 테스트 추가

--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -2,6 +2,10 @@ package com.example.api.service;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,4 +29,30 @@ class ApplyServiceTest {
 
 		assertThat(count).isEqualTo(1);
 	}
+
+	@Test
+	public void 여러명응모() throws InterruptedException {
+		int threadCount = 1000;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+		for (int i = 0; i < threadCount; i++) {
+			long userId = i;
+			executorService.submit(() -> {
+				try {
+					applyService.apply(userId);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+
+		latch.await();
+
+		long count = couponRepository.count();
+
+		assertThat(count).isEqualTo(100);
+	}
+
 }
