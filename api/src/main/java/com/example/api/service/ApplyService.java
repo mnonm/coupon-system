74ae705e79795387ab388e3c9e90c437f62278e9
songseongbin <ppsosqq@gmail.com>
2024-10-23 16:12:54 +0900

    [fix] ApplyService 미사용 코드 제거

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -2,22 +2,17 @@ package com.example.api.service;
 
 import org.springframework.stereotype.Service;
 
-import com.example.api.domain.Coupon;
 import com.example.api.producer.CouponCreateProducer;
 import com.example.api.repository.CouponCountRepository;
-import com.example.api.repository.CouponRepository;
 
 @Service
 public class ApplyService {
-
-	private final CouponRepository couponRepository;
 
 	private final CouponCountRepository couponCountRepository;
 
 	private final CouponCreateProducer couponCreateProducer;
 
-	public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
-		this.couponRepository = couponRepository;
+	public ApplyService(CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer) {
 		this.couponCountRepository = couponCountRepository;
 		this.couponCreateProducer = couponCreateProducer;
 	}
