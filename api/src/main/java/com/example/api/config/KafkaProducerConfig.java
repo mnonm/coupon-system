5273dcbbe4bf864fdc54db529f5243a95f68cc35
songseongbin <ppsosqq@gmail.com>
2024-10-23 16:03:59 +0900

    [fix] kafka producer config 수정

--- api/src/main/java/com/example/api/config/KafkaProducerConfig.java
+++ api/src/main/java/com/example/api/config/KafkaProducerConfig.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,7 +21,7 @@ public class KafkaProducerConfig {
 
 		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, Long.class);
+		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
 
 		return new DefaultKafkaProducerFactory<>(config);
 	}
